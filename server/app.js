2efb632c3660efe4d7854b9d9e18a284494e6212
hamzajaved70 <hamzajaved70@gmail.com>
2020-12-23 04:27:23 +1100

    graphql setup

--- server/app.js
+++ server/app.js
@@ -1,7 +1,16 @@
 // import express
 const express = require('express');
+// import express-graphql
+const { graphqlHTTP } = require('express-graphql');
+
 // initialize express
 const app = express();
+
+// create a graphql route
+app.use('/graphql', graphqlHTTP({
+    graphiql: true
+}));
+
 // set listening port with a callback function
 app.listen('4000', () => {
     console.log('Listening for requests on port 4000!');
